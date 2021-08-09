33f41718f4616ad52ed84fb966fe183298c7122f
Aditya Bimawan <aditditto@gmail.com>
2021-08-09 19:43:43 +0700

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '94d564f010c331a134149e6b5714b5ee'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9292a721-1324-41df-9fd5-6bbb8b65ed89'
+};
